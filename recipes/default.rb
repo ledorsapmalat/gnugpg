96079c71e0d56f273f290e3f1ed377effea7b73f
rodel-talampas <rodel.talampas@helixleisure.com>
2017-10-31 14:01:19 +1100

    Fixed new line issues

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,10 +5,10 @@
 # Copyright (c) 2017 Rodel M. Talampas, All Rights Reserved.
 
 case node['platform']
-  when 'debian', 'ubuntu'
-    include_recipe "gnugpg::_deb_install"
-  when 'redhat', 'centos', 'fedora'
-    include_recipe "gnugpg::_cent_install"
-  else # assume its windows
-    include_recipe "gnugpg::_win_install"
+when 'debian', 'ubuntu'
+  include_recipe 'gnugpg::_deb_install'
+when 'redhat', 'centos', 'fedora'
+  include_recipe 'gnugpg::_cent_install'
+else # assume its windows
+  include_recipe 'gnugpg::_win_install'
 end
