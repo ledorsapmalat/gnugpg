1349d895c6ab19633ef34121ccf990614645f4c0
rodel-talampas <rodel.talampas@helixleisure.com>
2017-10-31 14:18:16 +1100

    renamed to gnugpg

--- metadata.rb
+++ metadata.rb
@@ -1,4 +1,4 @@
-name 'gnupgp'
+name 'gnugpg'
 maintainer 'Rodel Manalo Talampas'
 maintainer_email 'rodel.talampas@gmail.com'
 license 'Apache-2.0'
@@ -10,5 +10,5 @@ chef_version '>= 12.5' if respond_to?(:chef_version)
 supports 'centos', '>= 6.2'
 supports 'amazon', '>= 2016.09'
 
-source_url 'https://github.com/rodel-talampas/gnupgp' if respond_to?(:source_url)
-issues_url 'https://github.com/rodel-talampas/gnupgp/issues' if respond_to?(:issues_url)
+source_url 'https://github.com/rodel-talampas/gnugpg' if respond_to?(:source_url)
+issues_url 'https://github.com/rodel-talampas/gnugpg/issues' if respond_to?(:issues_url)
